3bbd4b2c72d8f33f3adb8e19ba223cbcd550f920
wade <elvis122545735@gmail.com>
2023-05-17 10:25:46 +0800

    modify the herogame.php

--- herogame.php
+++ herogame.php
@@ -1,14 +1,16 @@
 <?php
 #create a hero staus
-$hp=100;
-$name='wade';
-$mp=1000;
-while($hp>0){
-    $random_num = mt_rand(-100, 0);
-    $array=["event: ".$random_num." HP", "name:".$name,"HP:".$hp."/100","MP:".$mp];
+$hp = 100;
+$name = 'wade';
+$mp = 1000;
+
+while($hp > 0){
+    $random_num = mt_rand(-10, 0);
+    $array=["event: ". $random_num. " HP", "name:". $name, "HP:". $hp. "/100", "MP:". $mp];
     echo implode("\n", $array);
     sleep(2);
     system('clear');
-        $hp=$hp+$random_num;
+    $hp = $hp + $random_num;
     }
+
 echo "Game over! \n";
